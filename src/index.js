a90835c92bd604eba974672c24e05967db0b1668
amdadul3036 <amdadul3036@gmail.com>
2021-01-09 15:26:18 +0600

    You cannot write javascript variable or constant directly inside a html file in JSX

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const FINE = "I am fine and you?";
+
 ReactDOM.render(
 
    <div>
     <h1>Hello Bro!! Ki obosthah?</h1>
-    <h2>I am Fine And You?</h2>
+    <h2>FINE</h2>
    </div>
   ,document.getElementById('root')
 );
