a704efa4af549ca194243c34584fc35ecfa60d15
amdadul3036 <amdadul3036@gmail.com>
2021-01-09 15:24:22 +0600

    render can render only one div or html tag

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom';
 
 ReactDOM.render(
 
-   <h1>Hello Bro!! Ki obosthah?</h1>
+   <div>
+    <h1>Hello Bro!! Ki obosthah?</h1>
+    <h2>I am Fine And You?</h2>
+   </div>
   ,document.getElementById('root')
 );
 
