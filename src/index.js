4563ff0b8847f9ab54eb274e468653eb4459a1a0
amdadul3036 <amdadul3036@gmail.com>
2021-01-10 17:15:56 +0600

    Card Creation

--- src/index.js
+++ src/index.js
@@ -6,16 +6,30 @@ const CALL = "Call Family";
 const Dummy = "Set up time ts is the time interval immidiately preeciding the active transition of the CLK signal during which the clock input must be maintained at the proper lavel "
 const Date = "09/01/2021";
 
+function CardName(){
+  return <div className = "FullCard">
+      <h1 className = "CallCard">{CALL}</h1>
+      <h3 className = "DummyCard">{Dummy}</h3>
+      <h4 className = "DateCard">{Date}</h4>
+  </div>
+}
 
 ReactDOM.render(
 
    <div>
-    <h1 style = {{backgroundColor : "blue" , fontSize: "100px" , textAlign: "center"}}>{APP}</h1>
-      <div className = "FullCard">
-          <h1 className = "CallCard">{CALL}</h1>
-          <h3 className = "DummyCard">{Dummy}</h3>
-          <h4 className = "DateCard">{Date}</h4>
-      </div>
+      <h1 style = {{backgroundColor : "blue" , fontSize: "100px" , textAlign: "center"}}>{APP}</h1>
+      <CardName />
+      <CardName />
+      <CardName />
+      <CardName />
+      <CardName />
+      <CardName />
+      <CardName />
+      <CardName />
+      <CardName />
+      <CardName />
+      <CardName />
+      <CardName />
    </div>
   ,document.getElementById('root')
 );
