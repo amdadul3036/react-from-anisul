12d3b8a5a4344b75594a967ff2e769c196f171da
amdadul3036 <amdadul3036@gmail.com>
2021-01-19 21:05:41 +0600

    Using a lot of Card just by a loop

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import Data from '../src/JSON DATA/data.json';
 const APP = "TO DO APP";
 
 function App(){
+
+  let items = [];
+  for (let x = 0; x < Data.length; x++) {
+    items.push(<CardName CallCard = {Data[x].title} Dummy = {Data[x].description} Date = "10/01/2021"/>) 
+  }
     return <div>
       <h1 style = {{backgroundColor : "blue" , fontSize: "100px" , textAlign: "center"}}>{APP}</h1>
-      <CardName CallCard = {Data[0].title} Dummy = {Data[0].description} Date = "10/01/2021"/>
-      <CardName CallCard = {Data[1].title} Dummy = {Data[1].description} Date = "10/02/2021"/>
-      <CardName CallCard = {Data[2].title} Dummy = {Data[2].description} Date = "10/03/2021"/>
-      <CardName CallCard = {Data[3].title} Dummy = {Data[3].description} Date = "10/04/2021"/>
-      <CardName CallCard = {Data[4].title} Dummy = {Data[4].description} Date = "10/05/2021"/>
-      <CardName CallCard = {Data[5].title} Dummy = {Data[5].description} Date = "10/06/2021"/>
+      {items}
     </div>
 }
 
