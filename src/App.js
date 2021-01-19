51b1ab68f4df71d29bb467de7e0fb8d7fa87c721
amdadul3036 <amdadul3036@gmail.com>
2021-01-19 21:23:19 +0600

    Map

--- src/App.js
+++ src/App.js
@@ -7,9 +7,12 @@ const APP = "TO DO APP";
 function App(){
 
   let items = [];
-  for (let x = 0; x < Data.length; x++) {
-    items.push(<CardName CallCard = {Data[x].title} Dummy = {Data[x].description} Date = "10/01/2021"/>) 
-  }
+  // for (let x = 0; x < Data.length; x++) {
+  //   items.push(<CardName CallCard = {Data[x].title} Dummy = {Data[x].description} Date = "10/01/2021"/>) 
+  // }  //In General we do not use for loop ; we use map
+
+  items = Data.map((item) => <CardName CallCard = { item.title } Dummy = {item.description} Date = {item.Date}/> )
+    
     return <div>
       <h1 style = {{backgroundColor : "blue" , fontSize: "100px" , textAlign: "center"}}>{APP}</h1>
       {items}
