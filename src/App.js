4f7e622be693d92068581b525709a5d9442a7bb8
amdadul3036 <amdadul3036@gmail.com>
2021-01-19 20:55:51 +0600

    JSON And it's use

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import CardName from '../src/Components/Card/card';
+import Data from '../src/JSON DATA/data.json';
 
 const APP = "TO DO APP";
 
 function App(){
     return <div>
       <h1 style = {{backgroundColor : "blue" , fontSize: "100px" , textAlign: "center"}}>{APP}</h1>
-      <CardName CallCard = "This is CallCard 1" Dummy = "This is Dummy Text 1" Date = "10/01/2021"/>
-      <CardName CallCard = "This is CallCard 1" Dummy = "This is Dummy Text 2" Date = "10/02/2021"/>
-      <CardName CallCard = "This is CallCard 1" Dummy = "This is Dummy Text 3" Date = "10/03/2021"/>
-      <CardName CallCard = "This is CallCard 1" Dummy = "This is Dummy Text 4" Date = "10/04/2021"/>
-      <CardName CallCard = "This is CallCard 1" Dummy = "This is Dummy Text 5" Date = "10/05/2021"/>
+      <CardName CallCard = {Data[0].title} Dummy = {Data[0].description} Date = "10/01/2021"/>
+      <CardName CallCard = {Data[1].title} Dummy = {Data[1].description} Date = "10/02/2021"/>
+      <CardName CallCard = {Data[2].title} Dummy = {Data[2].description} Date = "10/03/2021"/>
+      <CardName CallCard = {Data[3].title} Dummy = {Data[3].description} Date = "10/04/2021"/>
+      <CardName CallCard = {Data[4].title} Dummy = {Data[4].description} Date = "10/05/2021"/>
+      <CardName CallCard = {Data[5].title} Dummy = {Data[5].description} Date = "10/06/2021"/>
     </div>
 }
 
