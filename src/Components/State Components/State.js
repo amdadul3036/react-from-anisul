714bd657bf434e69c774b52869ee826c3ce78290
amdadul3036 <amdadul3036@gmail.com>
2021-01-23 22:24:10 +0600

    State

--- src/Components/State Components/State.js
+++ src/Components/State Components/State.js
@@ -1,10 +1,21 @@
 import React , {Component} from 'react';
 
 class StateLearning extends Component{
+
+    // We Need Constructure for STATE
+    constructor(props) {
+        super(props)
+
+        this.state = {
+                count : 0
+        }
+    }
+
+
     render(){
         return(
             <div>
-                <h2>{this.props.count}</h2>
+                <h2>Count : {this.state.count}</h2>
             </div>
         )
     }
